1a5f7b6e47868126c322256b31f5039d204cf288
majdal01 <145221922+majdal01@users.noreply.github.com>
2024-04-24 09:09:05 +0200

    Oprettet accordion med javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+//Accordian - reference: https://codepen.io/nera-iba/pen/VwRmvpz
+
+document.querySelectorAll('.accordion-item h2').forEach((accordionToggle) => { 
+    accordionToggle.addEventListener('click', () => { 
+    const accordionItem = accordionToggle.parentNode; 
+    const accordionContent = accordionToggle.nextElementSibling; 
+  
+     if (accordionContent.style.maxHeight) { 
+         accordionContent.style.maxHeight = null; 
+         accordionItem.classList.remove('active'); 
+        } else {
+          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'; 
+              accordionItem.classList.add('active'); 
+          }
+      });
+  });
