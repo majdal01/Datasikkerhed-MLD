1d61e0e7e561e10a82af601b2159f0d0e4762abb
majdal01 <145221922+majdal01@users.noreply.github.com>
2024-04-25 19:06:20 +0200

    Diverse rettelser

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ document.querySelectorAll('.accordion-item h3').forEach((accordionToggle) => {
           }
       });
   });
+
+
+  /*Hacking test*/
+
+    let inputfelt = document.getElementById('tbuser');
+    const btn = document.getElementById('btn');
+    let result = document.getElementById('result');
+
+    function test() {
+        if(inputfelt.toString().length > 4){
+            result.innerHTML = "Det tager en hacker 4 år at hacke dit password";
+        }else if(inputfelt.toString().length < 4){
+            result.innerHTML = "Det tager en hacker 0 år at hacke dit password";
+        }else{
+            result.innerHTML = "Indtast bogstaver, tal og symboler";
+            inputfelt.reset();
+        }
+        
+    }
+
+    btn.addEventListener('click', test);
+
+
+    /* Branching scenario*/
+
+  
+
+
+    
