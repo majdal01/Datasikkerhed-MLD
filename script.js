4ede5960d29868f798a7dab801ab0e488439bc05
majdal01 <145221922+majdal01@users.noreply.github.com>
2024-04-25 21:04:34 +0200

    Forsøg på javascript til branching scenario

--- script.js
+++ script.js
@@ -37,9 +37,70 @@ document.querySelectorAll('.accordion-item h3').forEach((accordionToggle) => {
     btn.addEventListener('click', test);
 
 
-    /* Branching scenario*/
+ /* Branching scenario*/
 
-  
+  const textElement = document.getElementById('text');
+  const optionButtonsElement = document.getElementById('option-buttons');
 
+  function startScenario() {
+    showTextNode(1);
+  }
+
+  function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex);
+    textElement.innerText = textNode.text;
+    while (optionButtonsElement.firstChild){
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+
+   textNode.options.forEach(option => {
+    if (showOption(option)) {
+    const button = document.createElement('button');
+    button.innerText = option.text;
+    button.classList.add('btn');
+    button.addEventListener('click', () => selectOption(option));
+    optionButtonsElement.appendChild(button);
+    }
+  })
+  }
+
+  function selectOption(option) {
+    const nextTextNodeId = option.nextText;
+    if (nextTextNodeId <= 0) {
+      return startScenario();
+    }
+    showTextNode(nextTextNodeId);
+  }
+
+  const textNodes = [
+    {
+        id: 1, 
+        text: 'Det er weekend og du har endelig tid til at sidde med din bærbare. Du skal både have fulgt op på din indbakke, finde forårstøj til børnene og meldt dig til sommerens hundeshow.',
+        /*text: 'Du starter med indbakken og opdager en mail, der ikke er til dig. Mailen ser mistænkelig ud og du får derfor lidt uro i hovedet. Du vælger at ringe til din tante, som er IT-ekspert og forklarer hende situationen ',
+        text: 'Hvad råder tanten dig til?',*/
+
+        options: [
+            {
+                text: 'Gå ind på hjemmesiden www.haveibeenpwned.com for at tjekke, om dit password er blevet hacket.',
+                nextText: 2
+             },
+             {
+                text: 'Skriv tilbage til personen der har sendt mailen for at afklare situationen.',
+                nextText: 2
+             },
+             {
+                text: 'Bloker afsenderen, slet mailen og skift password til mailkontoen med det samme.',
+                nextText: 2
+             }
+        ]
+    }, 
+    {
+        id: 2, 
+        Text: fffg,
+    }
+  ];
+
+  startScenario();
 
     
